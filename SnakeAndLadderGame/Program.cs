d8dc2b515b1c90803ac8846938cfe2970b84e1b7
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-09 23:34:28 +0530

    [Sunny] Add: Welcome to SnakeAndLadder

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System;
+namespace SnakeAndLadderGame
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome To SnakeAndLadder Games");
+        }
+    }
+}
+
